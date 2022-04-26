b075ae8887afef6e8646efc32a8fe853139ce064
kebin martinez sena <ajentek12@gmail.com>
2022-04-26 11:15:45 -0500

    arreglo paises

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//ruta de paises 
+Route::get('paises', function(){
+    $paises=[
+        "colombia" => [
+            "capital"=>" bogota",
+            "moneda"=>"peso",
+            "poblacion"=> 51.6,
+            "ciudades"=>[
+                "medellin",
+                "cali",
+                "barranquilla"
+
+        ]
+        ],
+        "peru" => [
+            "capital"=> "lima",
+            "moneda"=> "sol",
+            "poblacion"=> 32.97,
+            "ciudades"=>[
+                "cusco",
+                "arequipa",
+                "huancayo"
+
+            ]
+        ],
+        "paraguay" =>[
+            "capital"=> "asuncion",
+            "moneda"=> "guarani",
+            "poblacion"=> 7.133,
+            "ciudades"=>[
+                "ciudad del este",
+                "encarnacion",
+                "fuerte olimpo"
+            ]
+        ]
+    ];
+    //mostrar la vista de paises 
+    return view('paises')
+           ->with("paises" , $paises);
+
+} );
